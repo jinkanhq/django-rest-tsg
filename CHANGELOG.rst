19b1b721f7c201ceae027523cf2212ef83f70836
yinian1992 <yinian@jinkan.org>
2021-12-15 21:30:12 +0800

    release v0.1.1

---
+++ CHANGELOG.rst
@@ -0,0 +1,8 @@
+0.1.1
+-------------
+* Fix annotated trivial type fallback.
+* Skip ``typing`` types on dependency resolving.
+
+0.1.0
+-------------
+Initial version.
--- django_rest_tsg/__init__.py
+++ django_rest_tsg/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'Django REST TypeScript Generator'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Yinian Chin'
 __license__ = 'MIT License'
-__copyright__ = 'Copyright 2021 Jinkan'
+__copyright__ = 'Copyright 2021 Yinian Chin'
 
 VERSION = __version__
