483452f95a389422cf3a33b4d712f206cf548f68
yinian1992 <yinian@jinkan.org>
2022-04-18 21:07:42 +0800

    release v0.1.6

--- django_rest_tsg/__init__.py
+++ django_rest_tsg/__init__.py
@@ -1,5 +1,5 @@
 __title__ = "Django REST TypeScript Generator"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __author__ = "Yinian Chin"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021-2022 Yinian Chin"
